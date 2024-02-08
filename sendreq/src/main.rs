a478a3d641daac0d9706db65597c8f4e717b4177
forfd8960 <forfd8960@gmail.com>
2024-02-08 17:45:31 +0800

    Fix on_send_request implement

--- sendreq/src/main.rs
+++ sendreq/src/main.rs
@@ -3,17 +3,15 @@ slint::include_modules!();
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
 
-    ui.on_send_request(|method, request_url, header, body| {
+    let ui_handle = ui.as_weak();
+    ui.on_send_request(move |method, request_url, header, body| {
         println!("{method}");
         println!("{request_url}");
         println!("{header}");
         println!("{body}");
-        (move || {
-            let ui_handle = ui.as_weak();
-            let inner_ui = ui_handle.unwrap();
-            inner_ui.set_response(format!("{}: {}", method, request_url).into());
-        })();
-        // resp = format!("{}: {}", method, request_url);
+
+        let inner_ui = ui_handle.unwrap();
+        inner_ui.set_response(format!("{}: {}: {}", method, request_url, header).into());
     });
 
     // ui.set_response(resp.into());
